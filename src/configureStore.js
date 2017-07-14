54db4df3660929557ac62cd2062a217fdb947c67
Martin Babinec <M@rtinBabinec.cz>
2017-07-14 17:15:12 +0200

    created configureStore.js

---
+++ src/configureStore.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(sagaMiddleware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f,
+    ),
+  );
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App/';
+import configureStore from './configureStore';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App store={store} />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
