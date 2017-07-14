29db0aca51beee73a0774f385a896c82010f6d28
Martin Babinec <M@rtinBabinec.cz>
2017-07-14 17:15:12 +0200

    created root reducer

---
+++ src/reducers.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import dashboard from './components/Dashboard/reducers';
+import logIn from './components/LogIn/reducers';
+import signUp from './components/SignUp/reducers';
+import { LOAD_EVENTS_SUCCESS, LOAD_EVENTS_ERROR } from './components/Dashboard/actions';
+import { LOGOUT } from './components/LogIn/actions';
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case LOAD_EVENTS_SUCCESS:
+      return action.payload;
+    case LOAD_EVENTS_ERROR:
+    case LOGOUT:
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  events,
+  dashboard,
+  logIn,
+  signUp,
+});
