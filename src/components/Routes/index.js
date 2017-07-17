7fa34d07fdfe627206c08a92c389b47c73724066
Martin Babinec <M@rtinBabinec.cz>
2017-07-17 15:57:43 +0200

    added pages to Routes

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+import Dashboard from '../Dashboard';
+import Profile from '../Profile';
+import EventDetail from '../EventDetail';
+import EventNew from '../EventNew';
+import EventEdit from '../EventEdit';
 import LogIn from '../LogIn';
 import SignUp from '../SignUp';
-import Dashboard from '../Dashboard';
-import EventDetail from '../EventDetail';
 
 const Routes = ({ isLoggedIn }) => (
     isLoggedIn
@@ -13,7 +16,10 @@ const Routes = ({ isLoggedIn }) => (
         <div>
           <Switch>
             <Route path="/dashboard" exact component={Dashboard} />
+            <Route path="/profile" exact component={Profile} />
+            <Route path="/event/new" exact component={EventNew} />
             <Route path="/event/:id" exact component={EventDetail} />
+            <Route path="/event/:id/edit" exact component={EventEdit} />
             <Redirect to="/dashboard" />
           </Switch>
         </div>
