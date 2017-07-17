48cf7ecaf1e82b596a690e5a82d595c2ac631e48
Martin Babinec <M@rtinBabinec.cz>
2017-07-17 15:48:09 +0200

    added value prop to Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ label, error, name, type }) => (
+const Input = ({ label, error, name, type, value, ...others }) => (
   <div>
     <label htmlFor={name}>
       {label}
     </label>
-    <input name={name} type={type} />
+    <input name={name} type={type} defaultValue={value} {...others} />
     {error && <div>{error}</div>}
   </div>
 );
@@ -16,10 +16,12 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   error: PropTypes.string,
+  value: PropTypes.string,
 };
 
 Input.defaultProps = {
   error: '',
+  value: '',
 };
 
 export default Input;
