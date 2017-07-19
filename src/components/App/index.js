2104c63966c22fdc3aa4222455c10bdba8c3f42d
Martin Babinec <M@rtinBabinec.cz>
2017-07-19 15:38:32 +0200

    added loading notification after refresh

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { persistStore } from 'redux-persist-immutable';
+import configureStore from '../../configureStore';
 import Routes from '../Routes';
+import './index.css';
 
-const App = () => (
-  <div className="App">
-    <Routes />
-  </div>
-);
+const store = configureStore();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { rehydrated: false };
+  }
+
+  componentWillMount() {
+    persistStore(store, {}, () => {
+      this.setState({ rehydrated: true });
+    });
+  }
+
+  render() {
+    if (!this.state.rehydrated) return <div>Loading...</div>;
+
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/';
-import configureStore from './configureStore';
-import './index.css';
-
-const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App store={store} />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'));
+  <App />,
+  document.getElementById('root'),
+);
