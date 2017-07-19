99e49e14dab21a712a7a4b8a6f3eb6699b6448fa
Martin Babinec <M@rtinBabinec.cz>
2017-07-19 21:51:44 +0200

    added new selectors

--- src/selectors.js
+++ src/selectors.js
@@ -1,3 +1,5 @@
+import * as filterTypes from './constants/eventFilter';
+
 export const getEvents = state => state.getIn(['entities', 'events']);
 
 export const getUsers = state => state.getIn(['entities', 'users']);
@@ -5,6 +7,25 @@ export const getUsers = state => state.getIn(['entities', 'users']);
 export const getUserById = (state, id) => state.getIn(['entities', 'users', id]);
 
 export const getEventById = (state, id) => state.getIn(['entities', 'events', id]);
+
+export const getCurrentUser = state => state.getIn(['logIn', 'user']);
+
+export const getCurrentEventFilter = state => state.getIn(['filter', 'eventFilterType']);
+
+export const getFilteredEvents = (state, timestampNow) => {
+  const events = getEvents(state);
+  const filter = state.getIn(['filter', 'eventFilterType']);
+
+  if (filter === filterTypes.PAST) {
+    return events.filter(e => new Date(e.get('startsAt')).getTime() < timestampNow);
+  }
+
+  if (filter === filterTypes.FUTURE) {
+    return events.filter(e => new Date(e.get('startsAt')).getTime() > timestampNow);
+  }
+
+  return events;
+};
 
 export const getUserByEventAuthorId = (state, id) => {
   const event = getEventById(state, id);
